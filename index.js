a67a56f6543875e92252b9a58ee342761b724319
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2023-02-06 16:56:55 -0500

    add env CLOUDINARY_SECRET in index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ conectarDB()
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_NAME,
+  api_secret: process.env.CLOUDINARY_SECRET,
   secure: true
 })
 
